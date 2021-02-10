9b0885103db2def1adb88cbcd6b998c896b6ca1a
chrislane134 <christopherlane101@gmail.com>
2021-02-10 15:45:33 +0000

    added input. Simple input program.

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ print('Christopher Lane')
 print('0----')
 print(' ||||')
 print('*' * 10)
+
+patient_name = "John Smith"
+age = 20
+new_patient = True
+
+print('The patients name is ' + patient_name + 'is ' + (age) + 'years old')
