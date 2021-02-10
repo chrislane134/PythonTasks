2305d9f1976d808f67896ce723958d88749c4af6
chrislane134 <christopherlane101@gmail.com>
2021-02-10 16:55:08 +0000

    added simple input program while changing string type to int type

---
+++ type.py
@@ -0,0 +1,9 @@
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2021 - int(birth_year)
+# print(type(age))
+# print(age)
+
+weight = input('Weight: ')
+correct_weight = int(weight) * 0.45
+print(correct_weight + ('this is your weight in KG'))
