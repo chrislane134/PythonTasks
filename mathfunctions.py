e16285956a3d8b43e4711ea09b65a45cc62f9c4b
chrislane134 <christopherlane101@gmail.com>
2021-02-12 13:18:22 +0000

    explored maths module, and some functions

---
+++ mathfunctions.py
@@ -0,0 +1,8 @@
+import math  # import math module
+
+
+x = 2.9
+print(round(x))  # rounds number
+print(abs(-2.9))  # always returns a positive number.
+print(math.ceil(2.9))  # to get a ceiling of a number
+print(math.floor(2.9))  # to get a florr of a number.
