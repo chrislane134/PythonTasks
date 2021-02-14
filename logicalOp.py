0fac0f7d2c86fa86385f5b26094e4920cbfed749
chrislane134 <christopherlane101@gmail.com>
2021-02-14 17:29:59 +0000

    completed logical operators exercise. Finaly understood using the not operator!

---
+++ logicalOp.py
@@ -0,0 +1,10 @@
+has_good_credit = True
+criminal_record = False
+
+if has_good_credit and not criminal_record:
+    print("Eligible for Loan")
+
+
+# and : both
+# or : at least one
+# not : If you want something to be False use. Does the opposite.
